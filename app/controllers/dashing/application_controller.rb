50d19449336f3548ff3fcd61dcb782f2e910d48e
carpathiaxivvan <licheng.xu@coupa.com>
2016-08-03 13:57:10 -0700

    personalize gem

--- app/controllers/dashing/application_controller.rb
+++ app/controllers/dashing/application_controller.rb
@@ -1,56 +1,56 @@
-module Dashing
-  class ApplicationController < ActionController::Base
-    puts 'start newline2'
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts ''
-    puts 'end newline2'
-    before_filter :authentication_with_devise
-    before_action :user_authorized?
+# module Dashing
+#   class ApplicationController < ActionController::Base
+#     puts 'start newline2'
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts ''
+#     puts 'end newline2'
+#     before_filter :authentication_with_devise
+#     before_action :user_authorized?
 
-    rescue_from Octokit::NotFound, with: :force_sign_out
+#     rescue_from Octokit::NotFound, with: :force_sign_out
 
-    private
-    def user_authorized?
-      @user = session[:user]
-      return redirect_to authorization_path if @user.nil?
-    end
-
-
-    protected
-    def force_sign_out
-      redirect_to sign_out_path
-    end
+#     private
+#     def user_authorized?
+#       @user = session[:user]
+#       return redirect_to authorization_path if @user.nil?
+#     end
 
 
-    private
+#     protected
+#     def force_sign_out
+#       redirect_to sign_out_path
+#     end
 
-    def authentication_with_devise
-      Dashing.config.devise_allowed_models.each do |model|
-        self.send("authenticate_#{model.to_s}!")
-      end
-    end
 
-    def check_accessibility
-      auth_token = params.delete(:auth_token)
-      if !Dashing.config.auth_token || auth_token == Dashing.config.auth_token
-        true
-      else
-        render nothing: true, status: 401 and return
-      end
-    end
-  end
-end
+#     private
+
+#     def authentication_with_devise
+#       Dashing.config.devise_allowed_models.each do |model|
+#         self.send("authenticate_#{model.to_s}!")
+#       end
+#     end
+
+#     def check_accessibility
+#       auth_token = params.delete(:auth_token)
+#       if !Dashing.config.auth_token || auth_token == Dashing.config.auth_token
+#         true
+#       else
+#         render nothing: true, status: 401 and return
+#       end
+#     end
+#   end
+# end

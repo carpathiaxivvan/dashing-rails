c10c60ccf758e29eb96c308a49a08c9a3857a718
carpathiaxivvan <licheng.xu@coupa.com>
2016-08-03 14:37:05 -0700

    personalize gem

--- app/controllers/dashing/dashboards_controller.rb
+++ app/controllers/dashing/dashboards_controller.rb
@@ -1,60 +1,60 @@
-# module Dashing
-#   class DashboardsController < ApplicationController
-#     puts 'start newline2'
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts ''
-#     puts 'end newline2'
-#     before_action :verify_github_permission
-#     before_filter :check_dashboard_name, only: :show
+module Dashing
+  class DashboardsController < ApplicationController
+    puts 'start newline2'
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts 'end newline2'
+    before_action :verify_github_permission
+    before_filter :check_dashboard_name, only: :show
 
-#     rescue_from ActionView::MissingTemplate, with: :template_not_found
+    rescue_from ActionView::MissingTemplate, with: :template_not_found
 
-#     def index
-#       render file: dashboard_path(Dashing.config.default_dashboard || Dashing.first_dashboard || ''), layout: Dashing.config.dashboard_layout_path
-#     end
+    def index
+      render file: dashboard_path(Dashing.config.default_dashboard || Dashing.first_dashboard || ''), layout: Dashing.config.dashboard_layout_path
+    end
 
-#     def show
-#       render file: dashboard_path(params[:name]), layout: Dashing.config.dashboard_layout_path
-#     end
+    def show
+      render file: dashboard_path(params[:name]), layout: Dashing.config.dashboard_layout_path
+    end
 
-#     private
+    private
 
-#     def check_dashboard_name
-#       raise 'bad dashboard name' unless params[:name] =~ /\A[a-zA-z0-9_\-]+\z/
-#     end
+    def check_dashboard_name
+      raise 'bad dashboard name' unless params[:name] =~ /\A[a-zA-z0-9_\-]+\z/
+    end
 
-#     def dashboard_path(name)
-#       Dashing.config.dashboards_views_path.join(name)
-#     end
+    def dashboard_path(name)
+      Dashing.config.dashboards_views_path.join(name)
+    end
 
-#     def template_not_found
-#       raise "Count not find template for dashboard '#{params[:name]}'. Define your dashboard in #{dashboard_path('')}"
-#     end
+    def template_not_found
+      raise "Count not find template for dashboard '#{params[:name]}'. Define your dashboard in #{dashboard_path('')}"
+    end
 
-#     def verify_github_permission
-#       client = Octokit::Client.new(:client_id => ENV['GITHUB_CLIENT_ID'], :client_secret => ENV['GITHUB_CLIENT_SECRET'])
-#       puts "before nil***********"
-#       puts @user.nil?
-#       puts @user.uid
-#       puts @user.name
-#       puts @user.login
-#       puts @user.token
-#       puts "after nil*********"
-#       client.check_application_authorization(@user.token)
-#   end
-#   end
-# end
+    def verify_github_permission
+      client = Octokit::Client.new(:client_id => ENV['GITHUB_CLIENT_ID'], :client_secret => ENV['GITHUB_CLIENT_SECRET'])
+      puts "before nil***********"
+      puts @user.nil?
+      puts @user.uid
+      puts @user.name
+      puts @user.login
+      puts @user.token
+      puts "after nil*********"
+      client.check_application_authorization(@user.token)
+  end
+  end
+end
